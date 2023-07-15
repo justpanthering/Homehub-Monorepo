5f55f2196eba208c7a1d6aff26478fa39862e80a
Ankit Lakra <ankit.lakra@datapeace.in>
2023-07-15 10:36:37 +0530

    chore(backend-e2e): added test for /flighttracker

---
+++ packages/backend-e2e/src/backend/routes/flightTracking.spec.ts
@@ -0,0 +1,12 @@
+import axios from 'axios';
+
+describe('GET /flighttracker', () => {
+  it('should return a message', async () => {
+    const res = await axios.get(`/flighttracker`);
+
+    expect(res.status).toBe(200);
+    expect(res.data).toEqual(
+      expect.objectContaining({ success: true, data: expect.any(Array) })
+    );
+  });
+});
